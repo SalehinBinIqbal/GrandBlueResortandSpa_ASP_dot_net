2fde6c0b6b08fed9d834c2b8fb7a6a58840d815a
azizTarik-ui <69195041+azizTarik-ui@users.noreply.github.com>
2022-02-27 23:20:44 +0600

    Fetch

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GrandBlueResortandSpa.Models;
 
 namespace GrandBlueResortandSpa.Controllers
 {
     public class ReservationController : Controller
     {
+        GrandBlueEntities grandBlue = new GrandBlueEntities();
         // GET: Reservation
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(RoomBooking roomBooking)
+        {
+            grandBlue.RoomBookings.Add(roomBooking);
+            grandBlue.SaveChanges();
+            return View();
+        }
         public ActionResult Book()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Book(BOOKING booking)
+        {
+            grandBlue.BOOKINGs.Add(booking);
+            grandBlue.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
